ef79c15e91b624b939d9fcb4d06802454ed9da00
Zach Morgan <zachmorgan97@gmail.com>
2022-02-24 23:50:38 +0000

    feb 24

--- RB120/medium_1/highest_and_lowest_ranking_cards_2.rb
+++ RB120/medium_1/highest_and_lowest_ranking_cards_2.rb
@@ -4,12 +4,13 @@ class Card
   include Comparable
   
   FACE_VALUES = {"Jack" => 11, "Queen" => 12, "King" => 13, "Ace" => 14}
+  SUIT_VALUE = {"Spades" => 4, "Hearts" => 3, "Clubs" => 2, "Diamonds" => 1}
   
   attr_reader :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
-    @suit = suit  
+    @suit = suit  # -->> spades > hearts > clubs > diamonds
   end
   
   def numeric(rank)
@@ -18,7 +19,9 @@ class Card
   end
   
   def <=>(other_card)
-      numeric(rank) <=> other_card.numeric(other_card.rank)
+      num = numeric(rank) <=> other_card.numeric(other_card.rank)
+      return num unless num == 0
+      SUIT_VALUE[suit] <=> SUIT_VALUE[other_card.suit]
   end
   
   def to_s
@@ -26,31 +29,14 @@ class Card
   end
 end
 
-cards = [Card.new(2, 'Hearts'),
-         Card.new(10, 'Diamonds'),
-         Card.new('Ace', 'Clubs')]
-puts cards
-puts cards.min == Card.new(2, 'Hearts')
-puts cards.max == Card.new('Ace', 'Clubs')
-
-cards = [Card.new(5, 'Hearts')]
-puts cards.min == Card.new(5, 'Hearts')
-puts cards.max == Card.new(5, 'Hearts')
-
-cards = [Card.new(4, 'Hearts'),
-         Card.new(4, 'Diamonds'),
-         Card.new(10, 'Clubs')]
-puts cards.min.rank == 4
-puts cards.max == Card.new(10, 'Clubs')
-
 cards = [Card.new(7, 'Diamonds'),
          Card.new('Jack', 'Diamonds'),
          Card.new('Jack', 'Spades')]
 puts cards.min == Card.new(7, 'Diamonds')
-puts cards.max.rank == 'Jack'
+puts cards.max == Card.new('Jack', 'Spades')
 
 cards = [Card.new(8, 'Diamonds'),
          Card.new(8, 'Clubs'),
          Card.new(8, 'Spades')]
-puts cards.min.rank == 8
-puts cards.max.rank == 8
+puts cards.min == Card.new(8, 'Diamonds')
+puts cards.max == Card.new(8, 'Spades')
